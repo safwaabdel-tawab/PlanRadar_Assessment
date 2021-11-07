e77913d4b89a2d1cb108841998cd24c3758d282d
Safwa <safwa.tawab@gmail.com>
2021-11-07 23:55:50 +0200

    Adding frame rate to the scene

---
+++ Assignment_UnityDeveloper/Assets/Scripts/FPS_Display.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class FPS_Display : MonoBehaviour
+{
+    public Text FpsText;
+
+    private float pollingTime = 1f;
+    private float time;
+    private int frameCount;
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        frameCount++;
+        if(time >= pollingTime)
+        {
+            int frameRate = Mathf.RoundToInt(frameCount/time);
+            FpsText.text = frameRate.ToString() + " FPS";
+
+            time -= pollingTime;
+            frameCount = 0;
+        }
+    }
+}
