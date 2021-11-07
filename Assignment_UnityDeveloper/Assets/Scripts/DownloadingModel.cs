cbe2f0af2c2c0a76881081db138c459891e7c4ae
Safwa <safwa.tawab@gmail.com>
2021-11-07 18:25:50 +0200

    Making script for detecting finishing download

---
+++ Assignment_UnityDeveloper/Assets/Scripts/DownloadingModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Checking downloading process.
+/// </summary>
+public class DownloadingModel : MonoBehaviour
+{
+    public bool DownloadFinished
+    {
+        set; get;
+    }
+    void Start()
+    {
+        DownloadFinished = false;
+        StartCoroutine(StillDownloading());
+    }
+
+    IEnumerator StillDownloading()
+    {
+        int count = transform.childCount;
+        yield return new WaitUntil(() => count > 0);
+
+        DownloadFinished = true;
+    }
+}
