6dc31e702ee95dd1d997dec0b7d7eac90e47c7fe
Safwa <safwa.tawab@gmail.com>
2021-11-07 22:09:40 +0200

    Lerping rotation

--- Assignment_UnityDeveloper/Assets/Scripts/LerpCamera.cs
+++ Assignment_UnityDeveloper/Assets/Scripts/LerpCamera.cs
@@ -10,20 +10,29 @@ public class LerpCamera : MonoBehaviour
     public float lerpTime_Pos;
     public float lerpTime_Rotat;
 
-    private Vector3 endPosition;
-    private Vector3 startPosition;
+    private Vector3 targetPos;
+    private Vector3 startPos;
+
+    private Quaternion targetRotat;
+    private Quaternion startRotat;
+
     private bool shouldLerp = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        endPosition = new Vector3(InitialPos.position.x, InitialPos.position.y, InitialPos.position.z);
+        targetPos = InitialPos.position;
+        targetRotat = InitialPos.rotation;
+
+        //endPosition = new Vector3(InitialPos.position.x, InitialPos.position.y, InitialPos.position.z);
         //StartLerping();
     }
 
     public void StartLerping()
     {
-        startPosition = transform.position;
+        startPos = transform.position;
+        startRotat = transform.rotation;
+
         timeStartedLerping = Time.time;
         shouldLerp = true;
     }
@@ -33,17 +42,29 @@ public class LerpCamera : MonoBehaviour
     {
         if (shouldLerp)
         {
-            transform.position = Lerp(startPosition, endPosition, timeStartedLerping, lerpTime_Pos);
+            transform.position = Lerp_Pos(startPos, targetPos, timeStartedLerping, lerpTime_Pos);
+            transform.rotation = Lerp_Rotation(startRotat, targetRotat, timeStartedLerping, lerpTime_Pos);
 
-            if (transform.position == endPosition) shouldLerp = false;
+            if ((transform.position == targetPos) &&
+                (transform.rotation == targetRotat)) 
+                shouldLerp = false;
         }
     }
 
-    public Vector3 Lerp(Vector3 start, Vector3 end, float timeStartedLerping, float lerpTime = 1)
+    public Vector3 Lerp_Pos(Vector3 start, Vector3 end, float timeStartedLerping, float lerpTime = 1)
     {
         float timeSinceStarted = Time.time - timeStartedLerping;
         float percentageComplete = timeSinceStarted / lerpTime;
         var result = Vector3.Lerp(start, end, percentageComplete);
+
+        return result;
+    }
+    
+    public Quaternion Lerp_Rotation(Quaternion start, Quaternion end, float timeStartedLerping, float lerpTime = 1)
+    {
+        float timeSinceStarted = Time.time - timeStartedLerping;
+        float percentageComplete = timeSinceStarted / lerpTime;
+        var result = Quaternion.Lerp(start, end, percentageComplete);
 
         return result;
     }
