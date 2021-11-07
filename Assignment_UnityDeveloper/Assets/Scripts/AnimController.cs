a68bf2d4a0bcd08772609f72e84f4c83b17f840a
Safwa <safwa.tawab@gmail.com>
2021-11-07 19:40:56 +0200

    Loading Scene

---
+++ Assignment_UnityDeveloper/Assets/Scripts/AnimController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimController : MonoBehaviour
+{
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim.Play("Loading");
+    }
+
+}
