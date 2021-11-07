6cfb477944e3d64bfb172e38cc3c789e9641d844
Safwa <safwa.tawab@gmail.com>
2021-11-07 03:43:59 +0200

    Rotating camera around target object

---
+++ Assignment_UnityDeveloper/Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    [SerializeField] private Camera camera;
+    [SerializeField] private Transform target;
+
+    private Vector3 previousPosition;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            previousPosition = camera.ScreenToViewportPoint(Input.mousePosition);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 direction = previousPosition - camera.ScreenToViewportPoint(Input.mousePosition);
+
+            // camera.transform.position = new Vector3();
+            camera.transform.position = target.position;
+            camera.transform.Rotate(new Vector3(1, 0, 0), direction.y * 180);
+            camera.transform.Rotate(new Vector3(0, 1, 0), -direction.x * 180, Space.World);
+            camera.transform.Translate(new Vector3(0, 0, -160));
+
+            previousPosition = camera.ScreenToViewportPoint(Input.mousePosition);
+
+        }
+    }
+}
